c382b9b090118de107cc5a7db04ab5f33a5a5eee
kwang329 <kwang329@gatech.edu>
2016-09-24 17:44:28 -0400

    Fixed deprecated commands

--- First Person Test/Assets/MouseLook.cs
+++ First Person Test/Assets/MouseLook.cs
@@ -43,8 +43,8 @@ public class MouseLook : MonoBehaviour {
 			rotationX = ClampAngle (rotationX, minimumX, maximumX);
 			rotationY = ClampAngle (rotationY, -rangeY, rangeY);
 
-			Quaternion xQuaternion = Quaternion.AxisAngle (Vector3.up, Mathf.Deg2Rad * rotationX);
-			Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
 
 			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
 		}
@@ -53,16 +53,16 @@ public class MouseLook : MonoBehaviour {
 			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
 			rotationX = ClampAngle (rotationX, minimumX, maximumX);
 
-			Quaternion xQuaternion = Quaternion.AxisAngle (Vector3.up, Mathf.Deg2Rad * rotationX);
-			transform.localRotation = originalRotation * xQuaternion;
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
 		}
 		else
 		{
 			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
 			rotationY = ClampAngle (rotationY, -rangeY, rangeY);
 
-			Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
-			transform.localRotation = originalRotation * yQuaternion;
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
+            transform.localRotation = originalRotation * yQuaternion;
 		}
 	}
 
