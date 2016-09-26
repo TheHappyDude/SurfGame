0b82ff4dbeb67466d83d51458c07a932e2ff9481
kwang329 <kwang329@gatech.edu>
2016-09-26 00:44:06 -0400

    Add Vector stuff

---
+++ First Person Test/Assets/Scripts/VectorMath.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class VectorMath : MonoBehaviour {
+
+    private Vector3 acceleration;
+    private Vector3 velocity;
+    private float moveHorizontal;
+    private float moveVertical;
+    private float proj;
+    public float speedLimit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+        // Get key inputs
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        velocity = GetComponent<Rigidbody>().velocity;
+        acceleration = new Vector3(moveHorizontal, 0, moveVertical);
+        acceleration = Vector3.Normalize(acceleration);
+        proj = Vector3.Project(velocity, acceleration).magnitude;
+
+        if (proj <= speedLimit)
+        {
+            // Accelerate
+            velocity += acceleration * Time.fixedDeltaTime;
+        }
+
+    }
+}
